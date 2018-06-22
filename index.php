6c9957587f245741e7c6736ccec82f63aa8af975
JuliaPerezNeves <Juliap.n@hotmail.com>
2018-06-22 11:27:05 -0300

    Index

---
+++ index.php
@@ -0,0 +1,17 @@
+	
+	<?php include "cabecalho.php"; ?>
+		
+		<div id='conteudo'>
+				<p>
+					<h3>Vota Brasil</h3>
+				</p>
+				<p>		
+					Plataforma digital, destinada para as eleições de 2018.
+					</br>
+					Mostrando um Brasil mais tecnológico, votando sem sair de sua casa.
+					</br>
+					No presidente e/ou governador de  seu estado. 
+				</p>
+		</div>
+	</body>
+</html>
